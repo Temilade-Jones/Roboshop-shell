f3f5421b4325af05b99c1cfb53aba2afafe6cffb
temilade jones <temiladeajones@gmail.com>
2022-11-25 23:02:21 -0600

    Cart script

--- cart.sh
+++ cart.sh
@@ -1,49 +1,49 @@
 source common.sh
 
 PRINT "Install Nodejs Repos"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>LOG
 STAT $?
 
 PRINT "Install Nodejes"
-yum install nodejs -y
+yum install nodejs -y &>>LOG
 STAT $?
 
 PRINT "Adding application user"
-useradd roboshop
+useradd roboshop &>>LOG
 STAT $?
 
 PRINT "Download App content"
-curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>LOG
 STAT $?
 
 PRINT "Remove previous version of App"
-cd /home/roboshop
+cd /home/roboshop &>>LOG
 rm -rf cart
 STAT $?
 
 PRINT "Extracting App content"
-unzip -o /tmp/cart.zip
+unzip -o /tmp/cart.zip &>>LOG
 STAT $?
 
-mv cart-main cart
+mv cart-main cart &>>LOG
 cd cart
 
 PRINT "Install Nodejs dependencies for App"
-npm install
+npm install &>>LOG
 STAT $?
 
 PRINT "Configure endpoints for systemD configuration"
-sed -i -e 's/REDIS_ENDPOINT/redis.devopb69.online/' -e 's/CATALOGUE_ENPOINT/catalogue.devopsb69.online/'
+sed -i -e 's/REDIS_ENDPOINT/redis.devopb69.online/' -e 's/CATALOGUE_ENPOINT/catalogue.devopsb69.online/' &>>LOG
 STAT $?
 
 PRINT "Reload SystemD"
-systemctl daemon-reload
+systemctl daemon-reload &>>LOG
 STAT $?
 
 PRINT "Restart cart"
-systemctl restart cart
+systemctl restart cart &>>LOG
 STAT $?
 
 PRINT "Enable cart service"
-systemctl enable cart
+systemctl enable cart &>>LOG
 STAT $?
